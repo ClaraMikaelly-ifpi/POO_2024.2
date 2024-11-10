d4ef006771f97e699bd6bfd770e9e7a60dd63bd8
Clara <claramikaellyads.1@gmail.com>
2024-11-09 21:58:20 -0300

    atv02

---
+++ atvidade02/questao06.js
@@ -0,0 +1,9 @@
+"use strict";
+const nome = "Ely";
+const payment = 120.56;
+const languagem = "Typescript";
+const result = `${nome}
+My payment time is ${payment}
+and
+my preffered language is ${languagem}`;
+console.log(result);
---
+++ atvidade02/questao06.ts
@@ -0,0 +1,10 @@
+const nome = "Ely";
+const payment = 120.56;
+const languagem = "Typescript";
+
+const result = `${nome}
+My payment time is ${payment}
+and
+my preffered language is ${languagem}`;
+
+console.log(result);
